533c4686cd4e08c14a295d6568b9a0cf2b26621f
Carl-Anton Ingmarsson <ca.ingmarsson@gmail.com>
2014-02-09 22:47:33 +0100

    Algorithms: move Graph class definition to graph.h

--- C++/Algorithms/graph.cpp
+++ C++/Algorithms/graph.cpp
@@ -4,45 +4,7 @@
 #include <queue>
 #include <vector>
 
-template<typename T>
-class Graph
-{
-public:
-
-  struct Edge
-  {
-    int index;
-    int from, to;
-    T cost;
-
-    Edge(int i, int f, int t, T& w): index(i), from(f), to(t), cost(w) {}
-  };
-
-  Graph(int N): adj_(N), edgeIndex_(0) {}
-
-  int add_edge(int from, int to, T cost) {
-    adj_[from].push_back(Edge(edgeIndex_++, from, to, cost));
-    return edgeIndex_;
-  }
-
-  int N() const {
-    return adj_.size();
-  }
-  
-  const std::vector<Edge>& adj(int n) const {
-    return adj_[n];
-  }
-
-  void clear() {
-    for (int i = 0; i < N(); i++)
-      adj_[i].clear();
-    edgeIndex_ = 0;
-  }
-
-private:
-  std::vector< std::vector<Edge> > adj_;
-  int edgeIndex_;
-};
+#include "graph.h"
 
 template<typename T>
 class TopologicalSort
