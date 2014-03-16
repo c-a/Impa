c46810daf7c2d42328690e70ac0ccf1e6f01f287
Carl-Anton Ingmarsson <ca.ingmarsson@gmail.com>
2014-03-16 20:32:11 +0100

    Algorithms: Update Dijkstra implementation

--- C++/Algorithms/graph.cpp
+++ C++/Algorithms/graph.cpp
@@ -66,11 +66,11 @@ private:
 
   struct NodeData
   {
-    const Edge* edgeTo;
+    std::vector<const Edge*> edgesTo;
     T cost;
     bool visited;
 
-    NodeData() : edgeTo(0), visited(false) {}
+    NodeData() : visited(false) {}
   };
 
   struct Elem
@@ -90,21 +90,45 @@ private:
   };
 
 public:
+  Dijkstra() = default;
 
-  Dijkstra(const Graph<T>& G, int start): start_(start), D_(G.N()) {
+  void run(const Graph<T>& G, int start) {
+    D_.assign(G.N(), NodeData());
+    start_ = start;
     compute(G);
+  }
+
+  std::vector<Edge> get_shortest_edges(int goal) {
+    std::vector<Edge> edges;
+    std::vector<bool> seen(D_.size(), false);
+    std::queue<int> Q;
+    Q.push(goal);
+    seen[goal] = true;
+    while (!Q.empty()) {
+      int u = Q.front(); Q.pop();
+      for (const Edge* edge : D_[u].edgesTo) {
+        edges.push_back(*edge);
+        if (!seen[edge->from])
+          Q.push(edge->from);
+      }
+    }
+    return edges;
   }
 
   std::list<Edge> get_path(int goal) {
     std::list<Edge> path;
-    for (int cur = goal; D_[cur].edgeTo != 0; cur = D_[cur].edgeTo->from)
-      path.push_front(*D_[cur].edgeTo);
+    for (int cur = goal; D_[cur].edgesTo.size() > 0; cur = D_[cur].edgesTo[0]->from)
+      path.push_front(*D_[cur].edgesTo[0]);
 
     return path;
   }
 
   T get_cost(int goal) {
     return D_[goal].cost;
+  }
+
+  bool has_path(int goal) {
+    return D_[goal].visited;
   }
 
 private:
@@ -114,6 +138,7 @@ private:
   void compute(const Graph<T>& G) {
     std::priority_queue< Elem, std::vector<Elem>, std::greater<Elem> > Q;
 
+    D_[start_].cost = 0;
     Q.push(Elem(T(), start_));
     while (!Q.empty()) {
       Elem cur = Q.top();
@@ -128,11 +153,15 @@ private:
         if (D_[e.to].visited)
           continue;
 
-        if (D_[e.to].edgeTo == 0 ||
+        if (D_[e.to].edgesTo.size() == 0 ||
             cur.cost + e.cost < D_[e.to].cost) {
           D_[e.to].cost = cur.cost + e.cost;
-          D_[e.to].edgeTo = &e;
+          D_[e.to].edgesTo.clear();
           Q.push(Elem(D_[e.to].cost, e.to));
+        }
+        if (D_[e.to].edgesTo.size() == 0 ||
+            cur.cost + e.cost <= D_[e.to].cost) {
+            D_[e.to].edgesTo.push_back(&e);
         }
       }
 
@@ -257,9 +286,10 @@ int main() {
   assert(G.adj(2).size() == 1);
   assert(G.adj(2)[0].from == 2);
   assert(G.adj(2)[0].to == 3);
-  assert(G.edges().size() == 2);
+  assert(G.edges().size() == 3);
 
-  Dijkstra<int> di(G, 0);
+  Dijkstra<int> di;
+  di.run(G, 0);
 
   assert(di.get_cost(0) == 0);
   assert(di.get_cost(1) == 4);
@@ -271,4 +301,6 @@ int main() {
   assert(iter != path.end());
   assert((*iter).from == 0);
   assert((*iter).to == 1);
+
+  std::vector< Graph<int>::Edge > shortest_edges = di.get_shortest_edges(3);
 }
