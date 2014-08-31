242958d3238b3b70b35ca8972e8c0a9124a3b851
Carl-Anton Ingmarsson <ca.ingmarsson@gmail.com>
2014-08-31 20:25:08 +0200

    Add solution for #673 Parentheses Balance

---
+++ C++/ParenthesesBalance/uva-673.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+bool Correct(const std::string& line) {
+  std::vector<char> stack;
+  for (char c : line) {
+    if (c == '(' || c == '[') {
+      stack.push_back(c);
+    } else if (c == ')' || c == ']') {
+      if (stack.empty()) return false;
+      char back = stack.back();
+      if (c == ')' && back != '(') return false;
+      if (c == ']' && back != '[') return false;
+      stack.pop_back();
+    } else {
+      return false;
+    }
+  }
+  return stack.empty();
+}
+
+int main() {
+  int n;
+  std::cin >> n;
+  while (n-- > 0) {
+    std::string line;
+    std::cin >> line;
+    std::cout << (Correct(line) ? "Yes\n" : "No\n");
+  }
+  return 0;
+}
+
